ddeb1510d7dcaa6a44d5ebcf1bf501e5f1af6474
Anders Wood <anderswood@gmail.com>
2017-06-26 13:44:01 -0600

    updates

--- express-server.js
+++ express-server.js
@@ -1,11 +1,26 @@
 let express = require('express');
 let app = express();
 
-app.get('/', (req, res) => res.send('hello world'))
+app.use(express.static('public'));
 
-let server = app.listen(8081, () => {
-  let host = server.address().address;
-  let port = server.address().port;
+app.get('/express.htm', function (req, res) {
+   res.sendFile( __dirname + "/" + "express.htm" );
+})
 
-  console.log('Example app listening at http://%s:%s', host, port);
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+var server = app.listen(8081, function () {
+
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
 })
